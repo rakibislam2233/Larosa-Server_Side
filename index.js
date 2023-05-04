55c0cff1716c39d087402a86b5e791ac46c0f85f
Rakib500538 <rakib2020.tkg@gmail.com>
2023-05-04 20:04:15 +0600

    menu add

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors')
 const chefDetails = require('./chefDetails.json')
+const menu = require('./menu.json')
 app.use(cors())
 app.get('/', (req, res) => {
   res.send(chefDetails)
@@ -12,6 +13,9 @@ app.get('/chefDetails/:id', (req, res) => {
   const id = parseInt(req.params.id);
   const singleData = chefDetails.find(sd=> parseInt(sd.id) === id)
   res.send(singleData)
+})
+app.get('/menu', (req, res) => {
+  res.send(menu)
 })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
