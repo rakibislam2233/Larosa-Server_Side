996281b1e58ba6d644cb6a6ddab2270a581b1e17
Rakib500538 <rakib2020.tkg@gmail.com>
2023-05-04 13:33:46 +0600

    vercel.josn file add

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 const cors = require('cors')
 const chefDetails = require('./chefDetails.json')
 app.use(cors())
 app.get('/', (req, res) => {
   res.send(chefDetails)
 })
-
+app.get('/chefDetails/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const singleData = chefDetails.find(sd=> parseInt(sd.id) === id)
+  res.send(singleData)
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
